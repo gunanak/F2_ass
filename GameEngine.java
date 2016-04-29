52a8f333b7a16ee108ba2992f174576afc53375c
gunanak <dinosuer073@hotmail.com>
2016-04-29 19:59:52 +0700

    #08 โดนบล็อกชนแล้วจบเกม

--- GameEngine.java
+++ GameEngine.java
@@ -9,39 +9,39 @@ import java.util.Iterator;
 import javax.swing.Timer;
 
 public class GameEngine implements KeyListener, GameReporter{
-	GamePanel gp;
-	
-	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();	
-	private SpaceShip v;	
-	private Timer timer;
-	private long score = 0;
-	private double difficulty = 0.1;
+GamePanel gp;
+ 
+ 	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();	
+ 	private SpaceShip v;	
+ 	private Timer timer;
+ 	private long score = 0;
+ 	private double difficulty = 0.1;
 
-	public GameEngine(GamePanel gp, SpaceShip v) {
-		this.gp = gp;
-		this.v = v;		
-		gp.sprites.add(v);
-		
-		timer = new Timer(50, new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0){
-				process();
-			}
-		});
-		timer.setRepeats(true);
-		
-	}
-	public void start(){
-		timer.start();
-	}
-	private void generateEnemy(){
+public GameEngine(GamePanel gp, SpaceShip v) {
+ 		this.gp = gp;
+ 		this.v = v;		
+ 		gp.sprites.add(v);
+ 		
+ 		timer = new Timer(50, new ActionListener() {
+ 			@Override
+ 			public void actionPerformed(ActionEvent arg0){
+ 				process();
+ 			}
+ 		 });
+ 		timer.setRepeats(true);
+ 		
+ 	}
+ public void start(){
+ 		timer.start();
+ 	}
+ 	private void generateEnemy(){
 		Enemy e = new Enemy((int)(Math.random()*390), 30);
 		gp.sprites.add(e);
 		enemies.add(e);
 	}
-	
-	private void process(){
-		if(Math.random() < difficulty){
+ 
+ 	private void process(){
+ 		if(Math.random() < difficulty){
 			generateEnemy();
 		}
 
@@ -57,42 +57,54 @@ public class GameEngine implements KeyListener, GameReporter{
 			}
 		}
 
-		gp.updateGameUI(this);
+ 		gp.updateGameUI(this);
 
+ 		Rectangle2D.Double vr = v.getRectangle();
+		Rectangle2D.Double er;
+		for(Enemy e : enemies){
+			er = e.getRectangle();
+			if(er.intersects(vr)){
+				die();
+				return;
+			}
+		}
+ 	}
 
+ 	public void die(){
+		timer.stop();
+		
 	}
-
-	
-	void controlVehicle(KeyEvent e) {
+ 	
+ 	void controlVehicle(KeyEvent e) {
 		switch (e.getKeyCode()) {
-			case KeyEvent.VK_LEFT:
+		case KeyEvent.VK_LEFT:
 			v.move(-1);
 			break;
-			case KeyEvent.VK_RIGHT:
+		case KeyEvent.VK_RIGHT:
 			v.move(1);
 			break;
-			case KeyEvent.VK_D:
+		case KeyEvent.VK_D:
 			difficulty += 0.1;
 			break;
 		}
 	}
 
-	public long getScore(){
-		return score;
-	}
-	
-	@Override
-	public void keyPressed(KeyEvent e) {
-		controlVehicle(e);
-	}
-	
-	@Override
-	public void keyReleased(KeyEvent e) {
+ 	public long getScore(){
+ 		 return score;
+ 	}
+ 	
+ 	@Override
+ 	public void keyPressed(KeyEvent e) {
+ 		controlVehicle(e);
+ 	}
+ 
+ 	@Override
+ 	public void keyReleased(KeyEvent e) {
  		//do nothing
-	}
-	
-	@Override
-	public void keyTyped(KeyEvent e) {
+ 	}
+ 
+ 	@Override
+ 	public void keyTyped(KeyEvent e) {
  		//do nothing		
-	}
-}
+ 	}
+ }
